5c836e500c772f48c475eea63485071d0ba65c13
Tornike Kheladze <xeladze.tornike@gmail.com>
2022-07-12 16:42:21 +0400

    added react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route exact path="/" element={<h1>home</h1>} />
+      <Route path="/starred" element={<h1>starred</h1>} />
+    </Routes>
+  );
 }
 
 export default App;

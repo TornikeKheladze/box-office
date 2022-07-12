fe4a0aadb21ed9dab319c4a77022e719903c6d92
Tornike Kheladze <xeladze.tornike@gmail.com>
2022-07-12 20:43:45 +0400

    added search option

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,9 +5,15 @@ import { apiGet } from '../misc/config';
 const Home = () => {
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+
   const onSearch = () => {
-    apiGet(`/search/shows?q=${input}`).then(res => setResults(res));
+    apiGet(`/search/${searchOption}?q=${input}`).then(res => setResults(res));
   };
+  const onRadioChange = e => {
+    setSearchOption(e.target.value);
+  };
+  const isShowsSearch = searchOption === 'shows' ? true : false;
 
   const onInputChange = e => {
     setInput(e.target.value);
@@ -24,9 +30,12 @@ const Home = () => {
     if (results && results.length > 0) {
       return (
         <div>
-          {results.map(item => {
-            console.log(item);
-            return <div key={item.show.id}>{item.show.name}</div>;
+          {results.map(({ score, show, person }) => {
+            return show ? (
+              <div key={show.id}>{show.name}</div>
+            ) : (
+              <div key={person.id}>{person.name}</div>
+            );
           })}
         </div>
       );
@@ -42,6 +51,31 @@ const Home = () => {
         onKeyDown={onKeyDown}
         value={input}
       />
+      <div>
+        <label htmlFor="shows">
+          Shows
+          <input
+            type="radio"
+            id="shows"
+            name="searchQuery"
+            onChange={onRadioChange}
+            value="shows"
+            checked={isShowsSearch}
+          />
+        </label>
+
+        <label htmlFor="people">
+          Actors
+          <input
+            type="radio"
+            id="people"
+            name="searchQuery"
+            onChange={onRadioChange}
+            value="people"
+            checked={!isShowsSearch}
+          />
+        </label>
+      </div>
       <button onClick={onSearch}>Search</button>
       {renderResults()}
     </MainPageLayout>

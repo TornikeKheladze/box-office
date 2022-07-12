372d3f455bf2ba0cee1f625c0f7878ede44aa98a
Tornike Kheladze <xeladze.tornike@gmail.com>
2022-07-12 18:40:18 +0400

    created input and fetch results from tvmaze

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../Components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>Home</MainPageLayout>;
+  const [input, setInput] = useState('');
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(res => res.json())
+      .then(res => console.log(res));
+  };
+  const onInputChange = e => {
+    setInput(e.target.value);
+  };
+  const onKeyDown = e => {
+    if (e.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button onClick={onSearch}>Search</button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;

f0c595c55f91a9dc7ccdaeed41c55554ef8a4ed1
Tornike Kheladze <xeladze.tornike@gmail.com>
2022-07-13 15:49:34 +0400

    added show page

--- src/Components/show/Show.js
+++ src/Components/show/Show.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { apiGet } from "../../misc/config";
 
 const Show = () => {
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+  useEffect(() => {
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then((res) =>
+      setShow(res)
+    );
+  }, [id]);
+
   return <div>this is show page</div>;
 };
 

9a2a127ab74f689b31df76ce9fc1c844dda757a4
Tornike Kheladze <xeladze.tornike@gmail.com>
2022-07-16 17:43:29 +0400

    styled entire app

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,33 +1,40 @@
-import React, { useState } from 'react';
-import ActorGrid from '../Components/actor/ActorGrid';
-import MainPageLayout from '../Components/MainPageLayout';
-import ShowGrid from '../Components/show/ShowGrid';
-import { apiGet } from '../misc/config';
+import React, { useState } from "react";
+import ActorGrid from "../Components/actor/ActorGrid";
+import MainPageLayout from "../Components/MainPageLayout";
+import ShowGrid from "../Components/show/ShowGrid";
+import { SearchCard } from "../Components/styled";
+import { apiGet } from "../misc/config";
+import {
+  NoResultsWrapper,
+  RadioInputsWrapper,
+  SearchButtonWrapper,
+  SearchInput,
+} from "./Home.styled";
 
 const Home = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [results, setResults] = useState(null);
-  const [searchOption, setSearchOption] = useState('shows');
+  const [searchOption, setSearchOption] = useState("shows");
 
   const onSearch = () => {
-    apiGet(`/search/${searchOption}?q=${input}`).then(res => setResults(res));
+    apiGet(`/search/${searchOption}?q=${input}`).then((res) => setResults(res));
   };
-  const onRadioChange = e => {
+  const onRadioChange = (e) => {
     setSearchOption(e.target.value);
   };
-  const isShowsSearch = searchOption === 'shows' ? true : false;
+  const isShowsSearch = searchOption === "shows" ? true : false;
 
-  const onInputChange = e => {
+  const onInputChange = (e) => {
     setInput(e.target.value);
   };
-  const onKeyDown = e => {
+  const onKeyDown = (e) => {
     if (e.keyCode === 13) {
       onSearch();
     }
   };
   const renderResults = () => {
     if (results && results.length === 0) {
-      return <div>no results</div>;
+      return <NoResultsWrapper>no results</NoResultsWrapper>;
     }
     if (results && results.length > 0) {
       return results[0].show ? (
@@ -41,38 +48,43 @@ const Home = () => {
 
   return (
     <MainPageLayout>
-      <input
+      <SearchInput
         type="text"
         onChange={onInputChange}
         onKeyDown={onKeyDown}
         value={input}
       />
-      <div>
-        <label htmlFor="shows">
-          Shows
-          <input
-            type="radio"
-            id="shows"
-            name="searchQuery"
-            onChange={onRadioChange}
-            value="shows"
-            checked={isShowsSearch}
-          />
-        </label>
-
-        <label htmlFor="people">
-          Actors
-          <input
-            type="radio"
-            id="people"
-            name="searchQuery"
-            onChange={onRadioChange}
-            value="people"
-            checked={!isShowsSearch}
-          />
-        </label>
-      </div>
-      <button onClick={onSearch}>Search</button>
+      <RadioInputsWrapper>
+        <div>
+          <label htmlFor="shows">
+            Shows
+            <input
+              type="radio"
+              id="shows"
+              name="searchQuery"
+              onChange={onRadioChange}
+              value="shows"
+              checked={isShowsSearch}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="people">
+            Actors
+            <input
+              type="radio"
+              id="people"
+              name="searchQuery"
+              onChange={onRadioChange}
+              value="people"
+              checked={!isShowsSearch}
+            />
+          </label>
+        </div>
+      </RadioInputsWrapper>
+      <SearchButtonWrapper>
+        <button onClick={onSearch}>Search</button>
+      </SearchButtonWrapper>
       {renderResults()}
     </MainPageLayout>
   );
